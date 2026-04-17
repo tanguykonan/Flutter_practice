963f56e78cb62e6c1883f641b9488a2e0cb26320
Ulrich Tanguy <u.tanguykonan@gmail.com>
2026-04-17 09:37:55 +0000

    Gestion de package (dio-> introduction)

---
+++ lib/cours/exercices/gmailui/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:testfluter/cours/exercices/gmailui/screens/home_page.dart';
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: Colors.transparent,
+  ));
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const MyHomePage(title: "Gmail"),
+      },
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: false
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:testfluter/cours/exercices/gmailui/screens/home_page.dart';
+import 'package:dio/dio.dart';
 
 void main() {
 
@@ -21,12 +21,59 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: "/",
       routes: {
-        "/": (context) => const MyHomePage(title: "Gmail"),
+        "/": (context) => const MyHomePage(),
       },
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false
+      ),
+    );
+  }
+}
+
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List countries = [];
+  final dio = Dio();
+
+  void getCountries() async {
+    final response = await dio.get('https://restcountries.com/v3.1/all?fields=name,capital,flags');
+    setState(() {
+      countries = response.data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar( title: Text("Home Page"), elevation: 18.0,
+      actions: [
+        IconButton(onPressed: (){
+          getCountries();
+        }, icon: const Icon(Icons.refresh))
+      ],
+      ),
+      body: ListView.builder(
+        itemCount: countries.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(countries[index]["name"]["common"]),
+            subtitle: Text(countries[index]["name"]["official"]),
+            onTap: (){},
+          );
+        },
       ),
     );
   }
